518b2e5147be6342dd0ce7b32a1ebd69830762f7
edson.adriano <edsoneurides0705@gmail.cpm>
2024-05-15 19:52:53 -0300

    Create base repository interface

---
+++ aqrs_media.WebAPI/Interfaces/IBaseRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace aqrs_media.WebAPI.Interfaces
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        public Task<IEnumerable<T>?> GetAllAsync();
+        public Task<T?> GetByIdAsync(Guid id);
+
+        public Task<T> Create(T entity);
+        public Task<T> Update(T entity);
+        public Task<T> Delete(T entity);
+    }
+}
