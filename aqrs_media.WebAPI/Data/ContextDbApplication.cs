56b5b770396e358a04c5a472632a7e28aa008434
edson.adriano <edsoneurides0705@gmail.cpm>
2024-05-15 19:51:32 -0300

    Create context

---
+++ aqrs_media.WebAPI/Data/ContextDbApplication.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace aqrs_media.WebAPI.Data
+{
+    public class ContextDbApplication : DbContext
+    {
+        public ContextDbApplication(DbContextOptions<ContextDbApplication> options) : base(options)
+        {
+        }
+    }
+}
