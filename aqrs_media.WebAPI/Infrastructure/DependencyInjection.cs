130012974a7c211be28763cfdaacea1376b8d5ac
edson.adriano <edsoneurides0705@gmail.cpm>
2024-05-15 19:52:32 -0300

    Configure connection

---
+++ aqrs_media.WebAPI/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using aqrs_media.WebAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace aqrs_media.WebAPI.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration config)
+        {
+            var connection = config.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<ContextDbApplication>(options =>
+                options.UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 23))));
+
+            return services;
+        }
+    }
+}
