d5b01c1824b5ad4853525b467a080b5cd55f90e1
edson.adriano <edsoneurides0705@gmail.cpm>
2024-05-15 19:53:11 -0300

    Create Base Repository

---
+++ aqrs_media.WebAPI/Repositories/BaseRepository.cs
@@ -0,0 +1,70 @@
+﻿using aqrs_media.WebAPI.Data;
+using aqrs_media.WebAPI.Entities;
+using aqrs_media.WebAPI.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace aqrs_media.WebAPI.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : BaseEntity
+    {
+        protected readonly ContextDbApplication _context;
+
+        public BaseRepository(ContextDbApplication context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<T?> GetByIdAsync(Guid id)
+        {
+            return await _context 
+                            .Set<T>()
+                            .Where(t => t.Id == id)
+                            .SingleOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<T>?> GetAllAsync()
+        {
+            return await _context
+                            .Set<T>()
+                            .ToListAsync();
+        }
+
+
+        public async Task<T> Create(T entity)
+        {
+            _context
+                .Set<T>()
+                .Add(entity);
+
+            await _context
+                    .SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task<T> Update(T entity)
+        {
+            _context
+                .Set<T>()
+                .Update(entity);
+
+            await _context
+                    .SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task<T> Delete(T entity)
+        {
+            _context
+                .Set<T>()
+                .Remove(entity);
+
+            await _context
+                    .SaveChangesAsync();
+
+            return entity;
+        }
+    }
+}
