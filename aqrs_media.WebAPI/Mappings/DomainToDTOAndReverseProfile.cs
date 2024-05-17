be44ec14430bcac37d04a57306f7d8262e973258
edson.adriano <edsoneurides0705@gmail.cpm>
2024-05-17 20:47:27 -0300

    Mapping Media to Media DTO and reverse

---
+++ aqrs_media.WebAPI/Mappings/DomainToDTOAndReverseProfile.cs
@@ -0,0 +1,15 @@
+﻿using aqrs_media.WebAPI.DTOs;
+using aqrs_media.WebAPI.Entities;
+using AutoMapper;
+
+namespace aqrs_media.WebAPI.Mappings
+{
+    public class DomainToDTOAndReverseProfile : Profile
+    {
+        public DomainToDTOAndReverseProfile()
+        {
+            CreateMap<Media, MediaInsDTO>().ReverseMap();
+            CreateMap<Media, MediaPutDTO>().ReverseMap();
+        }
+    }
+}
