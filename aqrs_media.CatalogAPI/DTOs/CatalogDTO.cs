a8d48fb0adc55e5101e50448bf0613d3969e45cd
edson.adriano <edsoneurides0705@gmail.cpm>
2024-06-04 20:28:38 -0300

    Main DTOs

---
+++ aqrs_media.CatalogAPI/DTOs/CatalogDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace aqrs_media.CatalogAPI.DTOs
+{
+    public record CatalogDTO
+    {
+        public Guid Id { get; set; }
+        public Guid MediaId { get; set; }
+        public string MediaName { get; set; }
+        public double Price { get; set; }
+        public Guid MediaTypeId { get; set; }
+        public string Type {  get; set; }
+        public Guid CategoryId { get; set; }
+        public string Category { get; set; }
+        public Guid GenreId { get; set; }
+        public string Genre {  get; set; }
+        public Guid RatingId { get; set; }
+        public string Rating { get; set; } 
+        public List<ParticipantDTO> Participants { get; set; }
+    }
+}
---
+++ aqrs_media.CatalogAPI/DTOs/CatalogPutDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace aqrs_media.CatalogAPI.DTOs
+{
+    public record CatalogPutDTO
+    {
+        public Guid Id { get; set; }
+        public Guid MediaId { get; set; }
+        public double Price { get; set; }
+        public Guid MediaTypeId { get; set; }
+        public Guid CategoryId { get; set; }
+        public Guid GenreId { get; set; }
+        public Guid RatingId { get; set; }
+        public List<Guid> Participants { get; set; }
+    }
+}
