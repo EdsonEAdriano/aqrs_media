72090f3f80734b6aae2d0b8c066a2bb2a8e1ac66
edson.adriano <edsoneurides0705@gmail.cpm>
2024-06-04 20:29:54 -0300

    Apply pending migrations

---
+++ aqrs_media.CatalogAPI/Infrastructure/Migration.cs
@@ -0,0 +1,28 @@
+﻿using aqrs_media.CatalogAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace aqrs_media.CatalogAPI.Infrastructure
+{
+    public static class Migration
+    {
+        public static WebApplication ApplyPendingMigrations(this WebApplication app)
+        {
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<ContextDbApplication>();
+                    context.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError($"An error occurred while migrating the database. ERROR MESSAGE: {ex.Message};");
+                }
+            }
+
+            return app;
+        }
+    }
+}
