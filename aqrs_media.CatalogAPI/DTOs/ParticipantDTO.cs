52f9d3cc51d7f95b778129323493dbb765772263
edson.adriano <edsoneurides0705@gmail.cpm>
2024-06-25 21:01:09 -0300

    Move media api

--- aqrs_media.CatalogAPI/DTOs/ParticipantDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace aqrs_media.CatalogAPI.DTOs
-{
-    public record ParticipantDTO
-    {
-        public Guid ParticipantId { get; set; }
-        public string Name { get; set; }
-    }
-}
--- aqrs_media.CatalogAPI/Enums/EStatus.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace aqrs_media.CatalogAPI.Enums
-{
-    public enum EStatus
-    {
-        ACTIVE,
-        INACTIVE
-    }
-}
